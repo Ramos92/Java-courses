f23450f419bfb4343bed8c53fdf82bd793c624d4
BIG_TITS <hitachichi@yandex.ru>
2018-12-10 16:20:46 +0400

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,17 @@
 public class Calculate {
 
-      public static void main(String[] arg) {
-               System.out.println("Calculate...");
-
+      public static void main(String[] arg)   
+      {
+           System.out.println("Calculate...");
+           double first = Double.valueOf(arg[0]);
+           double second = Double.valueOf(arg[1]);
+           double summ = first + second;
+           double delenie = (double) first/second;
+           double stepen = Math.pow(first,second);
+           System.out.print("Bo3BeDeHuE B CTeneHb " + stepen + " ");
+           System.out.print("Delenie " + delenie + " ");
+           System.out.print("Sum " + summ);
       }
+
 
 }
